b1796ccb4e78b5cd6a1491cb5ba67228812f1c73
VileTung <kevin.wilbrink@googlemail.com>
2015-01-22 11:46:51 +0100

    Build cache is now selectable in run.php.

--- functions/lock.php
+++ functions/lock.php
@@ -1,124 +1,135 @@
 <?php
 
 /**
-* @author Kevin
-* @copyright 2014
-* @info Locks a session, also makes it stoppable
-*/
+ * @author Kevin
+ * @copyright 2014
+ * @info Locks a session, also makes it stoppable
+ */
 
 class locker
 {
-	private $session;
+    private $session;
 
-	//Constructor
-	public function __construct($process)
-	{
-		global $cache;
+    //Constructor
+    public function __construct($process)
+    {
+        global $cache;
 
-		//Generate session ID
-		$this->session = $session = mt_rand(10000, 65535);
+        //Generate session ID
+        $this->session = $session = mt_rand(10000, 65535);
 
-		//Create lock file
-		$sessionFile = fopen($cache . "lock_" . $session, "w");
-		fclose($sessionFile);
+        //Create lock file
+        $sessionFile = fopen($cache . "lock_" . $session, "w");
+        fclose($sessionFile);
 
-		//Make sure everything went OK
-		if (!file_exists($cache . "lock_" . $session))
-		{
-			throw new Exception("Couldn't create a lock!");
-		}
+        //Make sure everything went OK
+        if (!file_exists($cache . "lock_" . $session))
+        {
+            throw new Exception("Couldn't create a lock!");
+        }
 
-		//Database connection
-		Database();
+        //Database connection
+        Database();
 
-		sqlQueryi("INSERT INTO `sessions` (`process`,`pid`,`sessionId`,`progress`,`start`,`state`) VALUES (?,?,?,?,?,?)", array(
-		"siisss",
-		$process,
-		getmypid(),
-		$session,
-		"0",
-		date("Y-m-d H:i:s"),
-		"Working"));
-	}
+        sqlQueryi("INSERT INTO `sessions` (`process`,`pid`,`sessionId`,`progress`,`start`,`state`) VALUES (?,?,?,?,?,?)", array
+            (
+            "siisss",
+            $process,
+            getmypid(),
+            $session,
+            "0",
+            date("Y-m-d H:i:s"),
+            "Working"));
+    }
 
-	public function update($progress)
-	{
-		sqlQueryi("UPDATE `sessions` SET `progress` = ? WHERE `sessionId` = ?", array(
-		"si",
-		round($progress),
-		$this->session));
-	}
+    public function update($progress)
+    {
+        sqlQueryi("UPDATE `sessions` SET `progress` = ? WHERE `sessionId` = ?", array(
+            "si",
+            round($progress),
+            $this->session));
+    }
 
-	//Get session ID, don't know why..
-	public function getSession()
-	{
-		return $this->session;
-	}
+    //Get session ID, don't know why..
+    public function getSession()
+    {
+        return $this->session;
+    }
 
-	//Check if session.lock still exists, otherwise, exit!
-	public function check()
-	{
-		global $logging, $cache, $cacheExpire;
+    //Check if session.lock still exists, otherwise, exit!
+    public function check()
+    {
+        global $logging, $cache, $cacheExpire;
 
-		if (!file_exists($cache . "lock_" . $this->session))
-		{
-			//Update state
-			sqlQueryi("UPDATE `sessions` SET `state` = ?, `end` = ? WHERE `sessionId` = ?", array(
-			"ssi",
-			"Aborted",
-			date("Y-m-d H:i:s"),
-			$this->session));
-			
-			//Mark cache as 'old'
-			if (!touch($cacheExpire)) 
-			{
-				//Message
-				$logging->error("Failed to mark cache as old (" . $this->session . ")");
-			} 
-			else 
-			{
-				//Message
-				$logging->info("Marked cache as old (" . $this->session . ")");
-			}
+        if (!file_exists($cache . "lock_" . $this->session))
+        {
+            //Update state
+            sqlQueryi("UPDATE `sessions` SET `state` = ?, `end` = ? WHERE `sessionId` = ?", array(
+                "ssi",
+                "Aborted",
+                date("Y-m-d H:i:s"),
+                $this->session));
 
-			throw new Exception("Stopping, " . $this->session . ".lock doesn't exist");
-		}
-		else
-		{
-			//Message
-			$logging->debug("Process continues (" . $this->session . ")");
-		}
-	}
+            //Mark cache as 'old'
+            if (!touch($cacheExpire))
+            {
+                //Message
+                $logging->error("Failed to mark cache as old (" . $this->session . ")");
+            }
+            else
+            {
+                //Message
+                $logging->info("Marked cache as old (" . $this->session . ")");
+            }
 
-	//Regular exit
-	public function stop()
-	{
-		global $logging, $cache, $cacheExpire;
+            throw new Exception("Stopping, " . $this->session . ".lock doesn't exist");
+        }
+        else
+        {
+            //Message
+            $logging->debug("Process continues (" . $this->session . ")");
+        }
+    }
 
-		unlink($cache . "lock_" . $this->session);
-		
-		//Mark cache as 'old'
-		if (!touch($cacheExpire)) 
-		{
-			//Message
-			$logging->error("Failed to mark cache as old (" . $this->session . ")");
-		} 
-		else 
-		{
-			//Message
-			$logging->info("Marked cache as old (" . $this->session . ")");
-		}
+    //Regular exit
+    public function stop($buildCache = false)
+    {
+        global $logging, $cache, $cacheExpire;
 
-		//Update state
-		sqlQueryi("UPDATE `sessions` SET `state` = ?, `end` = ? WHERE `sessionId` = ?", array(
-		"ssi",
-		"Finished",
-		date("Y-m-d H:i:s"),
-		$this->session));
+        unlink($cache . "lock_" . $this->session);
 
-		//Message
-		$logging->info("Process stopped (" . $this->session . ")");
-	}
+        //Only mark as old when we are not building the cache
+        if (!$buildCache)
+        {
+            //Mark cache as 'old'
+            if (!touch($cacheExpire))
+            {
+                //Message
+                $logging->error("Failed to mark cache as old (" . $this->session . ")");
+            }
+            else
+            {
+                //Message
+                $logging->info("Marked cache as old (" . $this->session . ")");
+            }
+        }
+        //Build cache
+        else
+        {
+            //Message
+            $logging->info("Not marked as old, we're building cache (" . $this->session . ")");
+        }
+
+        //Update state
+        sqlQueryi("UPDATE `sessions` SET `state` = ?, `end` = ? WHERE `sessionId` = ?", array(
+            "ssi",
+            "Finished",
+            date("Y-m-d H:i:s"),
+            $this->session));
+
+        //Message
+        $logging->info("Process stopped (" . $this->session . ")");
+    }
 }
 
 ?>
--- run.php
+++ run.php
@@ -12,40 +12,62 @@ if (php_sapi_name() == "cli")
 {
     require_once ("functions/functions.php");
 
+    //Can we continue?
+    $continue = false;
+
     if (!isset($argv[1]))
     {
-        print ("Pick one: 'YTS': ");
+        print ("Pick one: 'YTS', 'buildCache': ");
         $retriever = trim(fgets(STDIN));
 
-        if ($retriever != "YTS")
+        if ($retriever != "YTS" && $retriever != "buildCache")
         {
             print ("\nWrong input!\n");
             exit;
         }
 
-        print ("Starting page: ");
-        $min = trim(fgets(STDIN));
-
-        if (!is_numeric($min))
+        //Only for YTS
+        if ($retriever == "YTS")
         {
-            print ("\nWrong input!\n");
-            exit;
-        }
+            print ("Starting page: ");
+            $min = trim(fgets(STDIN));
 
-        print ("Ending page: ");
-        $max = trim(fgets(STDIN));
+            if (!is_numeric($min))
+            {
+                print ("\nWrong input!\n");
+                exit;
+            }
 
-        if (!is_numeric($max))
-        {
-            print ("\nWrong input!\n");
-            exit;
+            print ("Ending page: ");
+            $max = trim(fgets(STDIN));
+
+            if (!is_numeric($max))
+            {
+                print ("\nWrong input!\n");
+                exit;
+            }
         }
 
         print ("\n");
         print ("Is this correct?\n");
         print ("Retriever: " . $retriever . "\n");
-        print ("Start: " . $min . "\n");
-        print ("End: " . $max . "\n");
+
+        //Only for YTS
+        if ($retriever == "YTS")
+        {
+            print ("Start: " . $min . "\n");
+            print ("End: " . $max . "\n");
+
+            //Locker text
+            $lText = "searching (" . $min . " until " . $max . ")";
+        }
+        //Only for buildCache
+        elseif ($retriever == "buildCache")
+        {
+            //Locker text
+            $lText = "building SQL Cache";
+        }
+
         print ("\n");
         print ("Typ 'yes' to continue: ");
 
@@ -58,21 +80,42 @@ if (php_sapi_name() == "cli")
         }
 
         print ("\n\n");
+
+        //Continue, execute
+        $continue = true;
     }
     //Automatic system
+    //YTS
     elseif ($argv[1] == "YTS" && isset($argv[2]) && isset($argv[3]) && is_numeric($argv[2]) && is_numeric($argv[3]))
     {
         $retriever = "YTS";
         $min = $argv[2];
         $max = $argv[3];
+
+        //Locker text
+        $lText = "(" . $min . " until " . $max . ")";
+
+        //Continue, execute
+        $continue = true;
+    }
+    //buildCache
+    elseif ($argv[1] == "buildCache")
+    {
+        $retriever = "buildCache";
+
+        //Locker text
+        $lText = "building SQL Cache";
+
+        //Continue, execute
+        $continue = true;
     }
 
-    if (isset($retriever) && isset($min) && isset($max))
+    if ($continue)
     {
         try
         {
             //Locks a session
-            $locker = new locker($retriever . " searching (" . $min . " until " . $max . ")");
+            $locker = new locker($retriever . " " . $lText);
 
             //Get created session
             $session = $locker->getSession();
@@ -81,16 +124,29 @@ if (php_sapi_name() == "cli")
             $logging = new loggen($log . $session);
 
             //Message
-            $logging->info("Starting '" . $retriever . "' (" . $min . " until " . $max . ")!");
+            $logging->info("Starting '" . $retriever . "' " . $lText . "!");
 
-            //Initialize YTS
-            $yts = new yts();
+            //YTS
+            if ($retriever == "YTS")
+            {
+                //Initialize YTS
+                $yts = new yts();
 
-            //Start YTS
-            $yts->movies($min, $max);
+                //Start YTS
+                $yts->movies($min, $max);
+            }
+            //buildCache
+            elseif ($retriever == "buildCache")
+            {
+                //Initialize getMovies
+                $getMovies = new getMovies();
+
+                //Build Cache
+                $getMovies->setCache();
+            }
 
             //Removes lock
-            $locker->stop();
+            $locker->stop($retriever == "buildCache" ? true : false);
         }
         //Error..
         catch (exception $e)
@@ -111,4 +167,5 @@ else
     die("I'm a CLI application!\n");
 }
 
+?>
 ?>
